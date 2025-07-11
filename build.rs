9f5e4af50e1d7d969d3c0c479be589599925c8f4
Emil Kovacev <Emil.Kovacev@ibm.com>
2025-07-11 22:39:58 +0000

    init qiskit_rs lib

--- build.rs
+++ build.rs
@@ -2,7 +2,9 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-search=qiskit_c_lib/dist/c/lib");
+    let curr_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rustc-env=LD_LIBRARY_PATH={curr_dir}/qiskit_c_lib/dist/c/lib");
+    println!("cargo:rustc-link-search={curr_dir}/qiskit_c_lib/dist/c/lib");
     println!("cargo:rustc-link-lib=qiskit");
 
     let bindings = bindgen::Builder::default()
