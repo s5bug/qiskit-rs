f5989dafb06ffddd9a222f037521b68d02d53f7e
Emil Kovacev <Emil.Kovacev@ibm.com>
2025-07-20 12:33:42 -0400

    Addtags to qiskit_ffi and remove ffi test from lib module

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,9 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
-
-
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
 mod qiskit_ffi;
 pub mod qiskit;
 
 pub use crate::qiskit::QuantumCircuit;
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-// 
-//     #[test]
-//     fn test_ghz() {
-//         unsafe {
-//             let num_qubits = 2;
-//             let circuit = qk_circuit_new(num_qubits, num_qubits);
-//             qk_circuit_gate(circuit, QkGate_QkGate_H, [0].as_ptr(), std::ptr::null());
-//             for i in 0..(num_qubits - 1) {
-//                 qk_circuit_gate(circuit, QkGate_QkGate_CX, [i, i + 1].as_ptr(), std::ptr::null());
-//             }
-//             for i in 0..num_qubits {
-//                 qk_circuit_measure(circuit, i, i);
-//             }
-//             qk_circuit_free(circuit);
-//         }
-//     }
-// }
