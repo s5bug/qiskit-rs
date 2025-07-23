59d5775525505819abf933ba15baeeef17c4af9a
Emil Kovacev <Emil.Kovacev@ibm.com>
2025-07-22 22:42:06 -0400

    Replaced single gate tests with parameterized tests

--- tests/test_gates.rs
+++ tests/test_gates.rs
@@ -1,15 +1,56 @@
 use qiskit_rs::{QuantumCircuit,QiskitError};
 
-
 #[test]
-fn test_dcx() {
-    let mut qc = QuantumCircuit::new(2, 0);
-    let ret = qc.dcx(0, 1);
-    assert_eq!(ret, QiskitError::Success)
+fn test_single_qubit_gates() {
+    let gate_funcs = [
+        QuantumCircuit::h,
+        QuantumCircuit::id,
+        QuantumCircuit::s,
+        QuantumCircuit::sdg,
+        QuantumCircuit::sx,
+        QuantumCircuit::sxdg,
+        QuantumCircuit::t,
+        QuantumCircuit::tdg,
+        QuantumCircuit::x,
+        QuantumCircuit::y,
+        QuantumCircuit::z
+    ];
+
+    for gate in gate_funcs {
+        let mut qc = QuantumCircuit::new(1, 0);
+        let ret = gate(&mut qc, 0);
+        assert_eq!(ret, QiskitError::Success);
+    }
 }
 
 #[test]
-fn test_ecr() {
-    let mut qc = QuantumCircuit::new(2, 0);
-    qc.ecr(0, 1);
+fn test_two_qubit_gates() {
+    let gate_funcs = [
+        QuantumCircuit::dcx,
+        QuantumCircuit::ecr,
+        QuantumCircuit::iswap,
+        QuantumCircuit::cx
+    ];
+
+    for gate in gate_funcs {
+        let mut qc = QuantumCircuit::new(2, 0);
+        let ret = gate(&mut qc, 0, 1);
+        assert_eq!(ret, QiskitError::Success);
+    }
+}
+
+#[test]
+fn test_single_param_gates() {
+    let gate_funcs = [
+        QuantumCircuit::p,
+        QuantumCircuit::rx,
+        QuantumCircuit::ry,
+        QuantumCircuit::rz
+    ];
+
+    for gate in gate_funcs {
+        let mut qc = QuantumCircuit::new(1, 0);
+        let ret = gate(&mut qc, 0.0, 0);
+        assert_eq!(ret, QiskitError::Success);
+    }
 }
